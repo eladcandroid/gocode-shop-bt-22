71fa293452ade0256761540bb341db9d70790411
Elad Cohen <elad@codicate.com>
2022-01-04 14:56:45 +0200

    State & Prop as function

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,8 +1,9 @@
 import "./Todo.css";
 
-export default function Todo({ id, title }) {
+export default function Todo({ id, title, completed, removeTodo }) {
   return (
-    <div className="Todo">
+    <div className={`Todo${completed ? ' completed' : ''}`}>
+      <button onClick={() => removeTodo(id)}>🗑️</button>
       Todo ID: {id} Title: {title}
     </div>
   );
