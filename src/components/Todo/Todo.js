8deb0162d99fcffba2a88b558cb3a64cf7758f16
Elad Cohen <eladc.android@gmail.com>
2022-01-11 16:52:23 +0200

    Add context

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,6 +1,15 @@
+import { useContext, useEffect } from "react";
+import TodosContext from "../../contexts/TodosContext";
 import "./Todo.css";
 
-export default function Todo({ id, title, completed, removeTodo }) {
+export default function Todo({ id, title, completed }) {
+  const {removeTodo} = useContext(TodosContext);
+  useEffect(() => {
+    // console.log('BORN');
+    return () => {
+      // console.log("DIED", id);
+    }
+  }, [])
   return (
     <div className={`Todo${completed ? ' completed' : ''}`}>
       <button onClick={() => removeTodo(id)}>🗑️</button>
